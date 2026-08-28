3d3b481ea046f0a5f866e7ad27f8e5edc911b6cc
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-08-28 13:55:08 +0530

    Add files via upload

---
+++ javascript/javascript Practice/first.js
@@ -0,0 +1,49 @@
+// let Num = prompt("Enter a Number");
+//  if(Num % 5 == 0)
+//  {
+//     console.log("Number is Multiple by 5");
+//  }
+// else{
+//     console.log("Number is Not Multiply by 5");
+// }
+
+/*let Score = prompt("Enter Student Score");
+
+if(Score >= 80)
+{
+    console.log("Student Have A Grade");
+}
+else if(Score >= 70 && Score <= 80)
+{
+    console.log("Student Have B Grade");
+}
+else if(Score >= 60 && Score <= 70)
+{
+    console.log("Student Have C Grade");
+}
+else if(Score >= 40 && Score <= 60)
+{
+    console.log("Student Have D Grade");
+}
+else{
+    console.log("Student Is Fail");
+}*/
+
+// for(let i=1;i < 100;i++)
+// {
+//     if(i % 2 == 0)
+//     {
+//         console.log("i =",i);
+//     }
+// }
+
+/*let gameNum = 15;
+
+let guessNum =prompt("Guess correct Number ");
+
+while(gameNum != guessNum)
+{
+    guessNum = prompt("You entered wrong Number.Guess correct Number");
+    
+}
+console.log("Congratulation you guess correct Number");*/
