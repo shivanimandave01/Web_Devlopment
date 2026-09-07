a273d18a078a8b99ac95ee7fbb7f431fb051b4c2
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-09-07 10:51:51 +0530

    Add files via upload

---
+++ javascript/Javascript Assignment Answer/01 Variables & Data Types/typeof.js
@@ -0,0 +1,13 @@
+let Number = 21;
+let name = "Rohit";
+let percentage = 90.5;
+let grade = 'A';
+let isPassed = true;
+let AccountNo = 9685743351567;
+console.log(typeof Number);
+console.log(typeof name);
+console.log(typeof percentage);
+console.log(typeof grade);
+console.log(typeof isPassed);
+console.log(typeof AccountNo);
+
